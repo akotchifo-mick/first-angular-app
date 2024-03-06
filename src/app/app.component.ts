c9472da4db315198ec59801f412641739a166c6e
Lordis Adjele <lordis.adjele@gmail.com>
2024-03-06 23:04:18 +0100

    sololearn-angular-notes-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet, HeaderComponent,MenuComponent],
+  //templateUrl: './app.component.html',
+  template: '<app-header />',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
